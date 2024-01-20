147410236dd93076c4cc46d766c58e2f05696b4c
harishpareek112 <harishpareek005@gmail.com>
2024-01-20 23:55:35 +0530

    first commit

--- factorial.cpp
+++ factorial.cpp
@@ -1,15 +1,23 @@
 //Find the factorial of a number
 #include<iostream>
 using namespace std;
+int factorial(int n){
+    int f;
+    if(n==0){
+        return 1;
+    }
+    else
+    {
+    f=n*factorial(n-1);
+    }
+    return f;
+
+}
 int main()
 {
-    int n; int factorial=1;
-    cout<<"Enter any number:";
-    cin>>n;
-    for(int i=0; i<n; i++)
-    {
-        factorial=n*factorial;
-    }
-    cout<<"\nFactorial of "<<n<<" is = "<<factorial;
+    int num;
+    cout << "Enter a Number: ";
+    cin>>num;
+    cout<<factorial(num);
     return 0;
 }
